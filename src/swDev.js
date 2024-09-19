06efe60a35ee0d516eb5131a597355a6796f51ec
Yogita Sawant <sawant.yogita1003@gmail.com>
2024-09-19 22:56:27 +0530

    resolved sw error - 01

--- src/swDev.js
+++ src/swDev.js
@@ -1,5 +1,5 @@
 export default function swDev() {
-    let swUrl = `${process.env.PUBLIC_URL || ""}/sw.js`; // Fallback to empty string if PUBLIC_URL is undefined
+    let swUrl = `/sw.js`; // Fallback to empty string if PUBLIC_URL is undefined
     navigator.serviceWorker.register(swUrl).then((response) => {
         console.warn("response", response)
     })
