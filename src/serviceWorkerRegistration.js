eb0d98192dc38cf4ae5b10723ca4b45babd0e13f
Yogita Sawant <sawant.yogita1003@gmail.com>
2024-10-04 12:21:03 +0530

    testing service worker-v1

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,10 +1,17 @@
-// serviceWorkerRegister.js
 export const registerServiceWorker = () => {
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-      .register("./sw.js")
-      .then((serviceWorker) => {
-        console.log("Service Worker registered: ", serviceWorker);
+      .register("/sw.js")
+      .then((registration) => {
+        console.log("Service Worker registered: ", registration);
+
+        if (registration.installing) {
+          console.log('Service worker installing');
+        } else if (registration.waiting) {
+          console.log('Service worker installed but waiting to activate');
+        } else if (registration.active) {
+          console.log('Service worker active');
+        }
       })
       .catch((error) => {
         console.error("Error registering the Service Worker: ", error);
